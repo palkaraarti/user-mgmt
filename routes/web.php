7821b468a951ac88f29e9a5d4844fb0fb8c77a3d
Aarti Palkar <aarti.palkar@medikabazaar.com>
2022-12-31 04:46:36 +0530

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('login', 'Auth\AuthController@index')->name('login');
+Route::post('post-login', 'Auth\AuthController@postLogin')->name('login.post'); 
+Route::get('registration', 'Auth\AuthController@registration')->name('register');
+Route::post('post-registration', 'Auth\AuthController@postRegistration')->name('register.post'); 
+Route::get('dashboard', 'Auth\AuthController@dashboard'); 
+Route::get('logout', 'Auth\AuthController@logout')->name('logout');
